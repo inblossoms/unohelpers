ff2309a530df122875d6a0b024498cbaaa442032
inblossoms <L13994352192@163.com>
2023-12-12 18:07:04 +0800

    refactor: update file path

--- src/utils/resend-req.js
+++ src/utils/resend-req.js
@@ -81,28 +81,3 @@ export class ReSend {
     });
   }
 }
-
-function fn1(params) {
-  return new Promise((resolve, reject) => {
-    return resolve("fn1 resolve.");
-  });
-}
-function fn2(params) {
-  return new Promise((resolve, reject) => {
-    return resolve("fn2 resolve.");
-  });
-}
-function fn3(params) {
-  return new Promise((resolve, reject) => {
-    return reject("fn3 reject.");
-  });
-}
-function fn4(params) {
-  return new Promise((resolve, reject) => {
-    return resolve("fn4 resolve.");
-  });
-}
-
-new ReSend([fn1, fn2, fn3, fn4], { tryMaxTimer: 3 })
-  .send()
-  .then((r) => console.log(r));
